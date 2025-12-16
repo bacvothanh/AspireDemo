8d24bdd0dcfa2bca455ea0366df826aa6d5973f0
Bac Thanh Vo <bac.thanh.vo@topicus.nl>
2025-12-16 13:45:11 +0700

    2 replicas

--- AspireDemo.AppHost/AppHost.cs
+++ AspireDemo.AppHost/AppHost.cs
@@ -3,6 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var kafka = builder.AddKafka("kafka")
     .WithKafkaUI();
 
-builder.AddProject<Projects.AspireDemo_API>("aspiredemo-api").WithReference(kafka);
+builder.AddProject<Projects.AspireDemo_API>("aspiredemo-api").WithReplicas(2).WithReference(kafka);
 
 builder.Build().Run();
