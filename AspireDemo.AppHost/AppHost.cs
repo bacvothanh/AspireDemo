1bc740291ae9b8b588c92b10b67d44f87c389a14
Bac Thanh Vo <bac.thanh.vo@topicus.nl>
2026-01-03 17:25:24 +0700

    wait for

--- AspireDemo.AppHost/AppHost.cs
+++ AspireDemo.AppHost/AppHost.cs
@@ -3,6 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var kafka = builder.AddKafka("kafka")
     .WithKafkaUI();
 
-builder.AddProject<Projects.AspireDemo_API>("aspiredemo-api").WithReplicas(2).WithReference(kafka);
+builder.AddProject<Projects.AspireDemo_API>("aspiredemo-api").WithReplicas(2).WithReference(kafka).WaitFor(kafka);
 
 builder.Build().Run();
